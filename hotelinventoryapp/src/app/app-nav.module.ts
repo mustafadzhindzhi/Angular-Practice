0e897334c95f2bd645f7d71bd5f5d457980521cc
Mustafa Dzhindzhi <dzindzimustafa@gmail.com>
2024-02-11 23:48:13 +0200

    angular

---
+++ hotelinventoryapp/src/app/app-nav.module.ts
@@ -0,0 +1,28 @@
+// app-nav.module.ts
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { LayoutModule } from '@angular/cdk/layout';
+
+import { AppNavComponent } from './app-nav/app-nav.component.js';
+
+@NgModule({
+  declarations: [
+    AppNavComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatSidenavModule,
+    LayoutModule,
+  ],
+  exports: [
+    AppNavComponent,
+  ],
+})
+export class AppNavModule {}
--- hotelinventoryapp/src/app/app.module.js.ts
+++ hotelinventoryapp/src/app/app.module.js.ts
@@ -2,7 +2,13 @@ import { APP_INITIALIZER, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http/index.js";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
 
 import { RoomsComponent } from "./rooms/rooms.component";
 import { RoomsListComponent } from "./rooms/rooms-list/rooms-list.component";
@@ -13,33 +19,50 @@ import { ContainerComponent } from "./container/container.component";
 import { RoomsService } from "./rooms/services/rooms.service";
 import { APP_CONFIG, APP_SERVICE_CONFIG } from "./AppConfig/appconfig.service";
 import { RequestInterceptor } from "./request.interceptor";
-import { InitService } from "./init.service.js";
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { InitService } from "./init.service";
+import { AppNavModule } from "./app-nav.module.js";
 
-function initFactory (InitService: InitService) {
+function initFactory(InitService: InitService) {
   return () => InitService.init();
 }
 
 @NgModule({
-  declarations: [AppComponent, RoomsComponent, RoomsListComponent, HeaderComponent, ContainerComponent, EmployeeComponent],
-  imports: [BrowserModule, AppRoutingModule, CommonModule, HttpClientModule], 
+  declarations: [
+    AppComponent,
+    RoomsComponent,
+    RoomsListComponent,
+    HeaderComponent,
+    ContainerComponent,
+    EmployeeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    AppNavModule, // Include AppNavModule
+  ],
   providers: [
     {
       provide: APP_SERVICE_CONFIG,
-      useValue: APP_CONFIG
+      useValue: APP_CONFIG,
     },
     {
       provide: HTTP_INTERCEPTORS,
-      useClass: RequestInterceptor ,
-      multi: true
+      useClass: RequestInterceptor,
+      multi: true,
     },
     {
       provide: APP_INITIALIZER,
       useFactory: initFactory,
       deps: [InitService],
-      multi: true
+      multi: true,
     },
-    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent],
 })
